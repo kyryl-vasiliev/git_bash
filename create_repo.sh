bc4bb48addd8ffba4c060817c8f3f1a7a026fb56
KyrylVasilev <andryasmo@gmail.com>
2020-12-17 05:30:36 +0200

    del unused string

--- create_repo.sh
+++ create_repo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 read username
 read token
-reponame='test1'
+reponame='test'
 curl \
  -X POST \
- -H "Authorization: token $token" https://api.github.com \
+ -H "Authorization: token $token"  \
  -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/user/repos \
  -d "{\"name\":\"$reponame\", \"auto_init\": true}"
