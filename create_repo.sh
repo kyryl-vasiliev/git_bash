2d96179f35217eb6ca58df349c5b0dad7d477889
KyrylVasilev <andryasmo@gmail.com>
2020-12-19 05:47:37 +0200

    add usability & help

--- create_repo.sh
+++ create_repo.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+
+if [ $# -eq 1 ]
+  then
+echo "You could fill username and token manually without pipe < "
+echo "Username as in path"
 read username
+echo "token"
 read token
-reponame='test'
+reponame=$1
+echo "trying to create $reponame"
 curl \
  -X POST \
  -H "Authorization: token $token"  \
  -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/user/repos \
  -d "{\"name\":\"$reponame\", \"auto_init\": true}"
+  else
+echo "usage ./create_repo.sh RepoName < UsernameAndTokenFile.txt"
+echo "	or"
+echo "	./create_repo.sh RepoName"
+echo "  sample of UsernameAndTokenFile.txt contest:"
+echo "	my_git_username"
+echo "	234645644578797897"
+fi
--- delete_repo.sh
+++ delete_repo.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-read name
+if [ $# -eq 1 ]
+  then
+echo "You could fill username and token manually without pipe < "
+echo "Username as in path"
+read username
+echo "token"
 read token
-reponame='test'
+reponame=$1
 
+echo "trying to delete $reponame if no output probably successful"
 curl \
   -X POST \
   -H "Authorization: token $token" \
   -X DELETE \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/$name/$reponame
+  https://api.github.com/repos/$username/$reponame
+  else
+echo "usage ./delete_repo.sh RepoNameToDelete < UsernameAndTokenFile.txt"
+echo "	or"
+echo " ./delete_repo.sh RepoNameToDelete"
+echo "  sample of UsernameAndTokenFile.txt contest:"
+echo "	my_git_username"
+echo "	234645644578797897"
+fi
+
